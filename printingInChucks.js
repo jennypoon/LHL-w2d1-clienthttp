60039f997262f3568b99a51e719ffa847bc6d0e2
Jenny Poon <jenny.poon@live.ca>
2018-10-01 18:26:13 +0000

    code to print data in chucks

--- printingInChucks.js
+++ printingInChucks.js
@@ -1,10 +1,27 @@
+//require http module
+var https = require('https');
+
 function getAndPrintHTMLChunks () {
 
+//defining host and path
   var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
+//callback responses
+https.get(requestOptions, function (response) {
+//encoding of recieving data
+  response.setEncoding('utf8');
+//when data received
+  response.on('data', function(data){
+    console.log(data, '/n');
+  });
+//all data received
+  response.on('end', function() {
+    console.log('Response stream complete.');
+  });
 
-  /* Add your code here */
-
+})
 }
+
+getAndPrintHTMLChunks()
